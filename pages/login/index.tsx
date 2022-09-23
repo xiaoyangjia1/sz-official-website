b7f281d2e1b091e491fca5667a994de0f1f4ddb9
yolokuku <YANGQIXIANG2001@163.com>
2022-09-23 11:33:29 +0800

    feat: 配置scss

---
+++ pages/login/index.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+import styles from "./login.module.scss";
+const Login: NextPage = () => {
+  return <div className={styles.test}>login</div>;
+};
+export default Login;
